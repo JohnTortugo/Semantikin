505b876dec39edbd467a1f0585549103c79a999e
divino.cesar <divcesar@gmail.com>
2015-10-23 21:36:30 -0200

    reaching defs

--- DataFlowAnalysis.h
+++ DataFlowAnalysis.h
@@ -9,7 +9,10 @@ namespace DFA {
 class ReachingDefinitions {
 	private:
 		BasicBlock_list_sptr _bbs;
+
 		std::map<SymbolTableEntry_sp, Instruction_list_sptr> _defs;
+		std::map<Instruction_sptr, Instruction_list_sptr> _gen;
+		std::map<Instruction_sptr, Instruction_list_sptr> _kill;
 
 	public:
 		ReachingDefinitions(BasicBlock_list_sptr bbs);
